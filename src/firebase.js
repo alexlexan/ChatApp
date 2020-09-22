a5097807f565a70145016db353f297726256dcf2
Алексей <milutkin.work@gmail.com>
2020-09-22 15:50:30 +0300

    deploy

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/database";
 import "firebase/storage";
 
 var config = {
-  apiKey: "AIzaSyBAr3_mO4aYNe8XWgabcyiEPFZx9WziZY8",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "react-slack-5327c.firebaseapp.com",
   databaseURL: "https://react-slack-5327c.firebaseio.com",
   projectId: "react-slack-5327c",
   storageBucket: "react-slack-5327c.appspot.com",
   messagingSenderId: "110127715168",
-  appId: "1:110127715168:web:c3b0abea5fa88e849b8405"
+  appId: "1:110127715168:web:c3b0abea5fa88e849b8405",
 };
 firebase.initializeApp(config);
 
